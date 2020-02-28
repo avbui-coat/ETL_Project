dd99a4db71ddb18f639f1c45d3535abf67d7fd76
An Bui <hvbuianbui@gmail.com>
2020-02-27 20:36:15 -0800

    update 2

---
+++ QuickDBD-Free Diagram-2.sql
@@ -0,0 +1,48 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/aKZvAB
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Case_country" (
+    "Country" TEXT   NOT NULL,
+    "date" DATE   NOT NULL,
+    "Confirmed" INT   NOT NULL,
+    "Deaths" INT   NOT NULL,
+    "Recovered" INT   NOT NULL
+);
+
+CREATE TABLE "TotalCase_location" (
+    "Location_id" INT   NOT NULL,
+    "Confirmed" INT   NOT NULL,
+    "Deaths" INT   NOT NULL,
+    "Recovered" INT   NOT NULL,
+    "Latest_date" Date   NOT NULL
+);
+
+CREATE TABLE "Geography" (
+    "Location_id" INT   NOT NULL,
+    "Country" TEXT   NOT NULL,
+    "StateProvince" TEXT   NOT NULL,
+    "Longitude" FLOAT   NOT NULL,
+    "Latitude" FLOAT   NOT NULL,
+    CONSTRAINT "pk_Geography" PRIMARY KEY (
+        "Location_id"
+     )
+);
+
+CREATE TABLE "Case" (
+    "Location_id" INT   NOT NULL,
+    "Date_reported" DATE   NOT NULL,
+    "StateProvince" TEXT   NOT NULL,
+    "Country" TEXT   NOT NULL,
+    "Confirmed" INT   NOT NULL,
+    "Deaths" INT   NOT NULL,
+    "Recovered" INT   NOT NULL
+);
+
+ALTER TABLE "TotalCase_location" ADD CONSTRAINT "fk_TotalCase_location_Location_id" FOREIGN KEY("Location_id")
+REFERENCES "Geography" ("Location_id");
+
+ALTER TABLE "Case" ADD CONSTRAINT "fk_Case_Location_id" FOREIGN KEY("Location_id")
+REFERENCES "Geography" ("Location_id");
+
